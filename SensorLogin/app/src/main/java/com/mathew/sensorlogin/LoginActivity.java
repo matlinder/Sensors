e677cb77c212f2c31268262daf4293a0468ba5af
matlinder <linder.mat@gmail.com>
2018-01-30 09:47:21 -0800

    start of project
    login in screen
    grabs json data, shows on screen
    need to parse

---
+++ SensorLogin/app/src/main/java/com/mathew/sensorlogin/LoginActivity.java
@@ -0,0 +1,157 @@
+package com.mathew.sensorlogin;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+import com.loopj.android.http.RequestParams;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import javax.xml.transform.Result;
+
+public class LoginActivity extends AppCompatActivity {
+    // Progress Dialog Object
+    ProgressDialog prgDialog;
+    // Error Msg TextView Object
+    TextView errorMsg;
+    // Email Edit View Object
+    EditText emailET;
+    // Password Edit View Object
+    EditText pwdET;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Find Error Msg Text View control by ID
+        errorMsg = (TextView) findViewById(R.id.login_error);
+        // Find Email Edit View control by ID
+        emailET = (EditText) findViewById(R.id.loginEmail);
+        // Find Password Edit View control by ID
+        pwdET = (EditText) findViewById(R.id.loginPassword);
+        // Instantiate Progress Dialog object
+        prgDialog = new ProgressDialog(this);
+        // Set Progress Dialog Text
+        prgDialog.setMessage("Please wait...");
+        // Set Cancelable as False
+        prgDialog.setCancelable(false);
+
+    }
+
+    /**
+     * Method gets triggered when Login button is clicked
+     *
+     * @param view
+     */
+    public void loginUser(View view) {
+        // Get Email Edit View Value
+        String email = emailET.getText().toString();
+        // Get Password Edit View Value
+        String password = pwdET.getText().toString();
+        // Instantiate Http Request Param Object
+        RequestParams params = new RequestParams();
+        // When Email Edit View and Password Edit View have values other than Null
+        if (Utility.isNotNull(email) && Utility.isNotNull(password)) {
+            // When Email entered is Valid
+
+                // Put Http parameter username with value of Email Edit View control
+                params.put("username", email);
+                // Put Http parameter password with value of Password Edit Value control
+                params.put("password", password);
+                // Invoke RESTful Web Service with Http parameters
+                invokeWS(params);
+            }
+            else {
+            Toast.makeText(getApplicationContext(), "Please fill the form, don't leave any field blank", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+    /**
+     * Method that performs RESTful webservice invocations
+     *
+     * @param params
+     */
+    public void invokeWS(RequestParams params) {
+        // Show Progress Dialog
+        prgDialog.show();
+        // Make RESTful webservice call using AsyncHttpClient object
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("https://www.imonnit.com/json/GetAuthToken", params, new AsyncHttpResponseHandler() {
+            // When the response returned by REST has Http response code '200'
+            @Override
+            public void onSuccess(String response) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                try {
+                    // JSON Object
+                    JSONObject obj = new JSONObject(response);
+                    Toast.makeText(getApplicationContext(), response, Toast.LENGTH_LONG).show();
+                    // When the JSON response has status boolean value assigned with true
+                    //obj.get()
+//                        Toast.makeText(getApplicationContext(), "auth token " + obj.getString("xsd"), Toast.LENGTH_LONG).show();
+//                        // Navigate to Home screen
+//                        navigatetoHomeActivity();
+                                        // Else display error message
+//                    else {
+//                        errorMsg.setText(obj.getString("error_msg"));
+//                        Toast.makeText(getApplicationContext(), obj.getString("error_msg"), Toast.LENGTH_LONG).show();
+//                    }
+                } catch (JSONException e) {
+                    // TODO Auto-generated catch block
+                    Toast.makeText(getApplicationContext(), "Error Occured [Server's JSON response might be invalid]!", Toast.LENGTH_LONG).show();
+                    e.printStackTrace();
+
+                }
+            }
+
+            // When the response returned by REST has Http response code other than '200'
+            @Override
+            public void onFailure(int statusCode, Throwable error,
+                                  String content) {
+                // Hide Progress Dialog
+                prgDialog.hide();
+                // When Http response code is '404'
+                if (statusCode == 404) {
+                    Toast.makeText(getApplicationContext(), "Requested resource not found", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code is '500'
+                else if (statusCode == 500) {
+                    Toast.makeText(getApplicationContext(), "Something went wrong at server end", Toast.LENGTH_LONG).show();
+                }
+                // When Http response code other than 404, 500
+                else {
+                    Toast.makeText(getApplicationContext(), "Unexpected Error occcured! [Most common Error: Device might not be connected to Internet or remote server is not up and running]", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
+    /**
+     * Method which navigates from Login Activity to Home Activity
+     */
+    public void navigatetoHomeActivity(){
+        Intent homeIntent = new Intent(getApplicationContext(),HomeActivity.class);
+        //homeIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(homeIntent);
+    }
+}
+
+
+
+
---
+++ SensorLogin/app/src/main/java/com/mathew/sensorlogin/Utility.java
@@ -0,0 +1,36 @@
+package com.mathew.sensorlogin;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+/**
+ * Class which has Utility methods
+ *
+ */
+public class Utility {
+    private static Pattern pattern;
+    private static Matcher matcher;
+    //Email Pattern
+    private static final String EMAIL_PATTERN = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+    /**
+     * Validate Email with regular expression
+     *
+     * @param email
+     * @return true for Valid Email and false for Invalid Email
+     */
+    public static boolean validate(String email) {
+        pattern = Pattern.compile(EMAIL_PATTERN);
+        matcher = pattern.matcher(email);
+        return matcher.matches();
+
+    }
+    /**
+     * Checks for Null String object
+     *
+     * @param txt
+     * @return true for not null and false for null String object
+     */
+    public static boolean isNotNull(String txt){
+        return txt!=null && txt.trim().length()>0 ? true: false;
+    }
+}
